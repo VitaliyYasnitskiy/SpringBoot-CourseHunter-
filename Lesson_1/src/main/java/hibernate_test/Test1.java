cf10de1a2d11227325698e628664639b9c121131
Yasnitskiy Vitaliy <vitaliyyas1981@gmail.com>
2024-01-05 14:40:33 +0500

    040 Сохранение Java объектов в БД.mp4

---
+++ Lesson_1/src/main/java/hibernate_test/Test1.java
@@ -0,0 +1,29 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args){
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+        Employee emp = new Employee("Vitaliy","Tregulov"
+                ,"IT",500);
+       try {
+           session.beginTransaction();
+          session.save(emp);
+//           session.persist(emp);
+           session.getTransaction().commit();
+       }
+       finally {
+           factory.close();
+       }
+
+    }
+}
