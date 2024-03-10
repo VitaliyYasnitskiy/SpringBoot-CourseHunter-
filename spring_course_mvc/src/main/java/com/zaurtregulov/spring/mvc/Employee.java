69a38f567466935696d0fb2ec4d2896ddaaef7aa
Yasnitskiy Vitaliy <vitaliyyas1981@gmail.com>
2024-03-10 21:59:34 +0500

    062 Spring MVC form  select

--- spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/Employee.java
+++ spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/Employee.java
@@ -1,12 +1,21 @@
 package com.zaurtregulov.spring.mvc;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Employee {
     private String name;
     private String surname;
+    private Map<String, String> departments;
+
     private int salary;
     private String department;
 
     public Employee() {
+        departments = new HashMap<>();
+        departments.put("IT","Information Technology");
+        departments.put("HR","Human Resourses");
+        departments.put("Sales","Sales");
+
     }
 
     public String getName() {
@@ -39,6 +48,14 @@ public class Employee {
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    public Map<String, String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Map<String, String> departments) {
+        this.departments = departments;
     }
 
     @Override
